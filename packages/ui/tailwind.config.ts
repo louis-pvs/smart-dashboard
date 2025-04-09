2f6ef0d43a46c2cb5c0e18fb0f05a67fb77b7d5a
Louis Phang <lop@topdanmark.dk>
2025-04-09 10:38:38 +0800

    Refactor code structure for improved readability and maintainability

--- packages/ui/tailwind.config.ts
+++ packages/ui/tailwind.config.ts
@@ -2,9 +2,8 @@ import type { Config } from "tailwindcss";
 import sharedConfig from "@repo/tailwind-config";
 
 const config: Pick<Config, "prefix" | "presets" | "content"> = {
-  content: ["./src/**/*.tsx"],
-  prefix: "ui-",
   presets: [sharedConfig],
+  content: ["./src/**/*.tsx"],
 };
 
 export default config;
